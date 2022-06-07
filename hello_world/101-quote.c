c7c8acbb10ca820497cbfe263d90dde2aaab3a56
Salvador Borit <5084@holbertonschool.com>
2022-06-06 18:58:28 -0700

    8.5

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fprintf(stderr, "%s\n", str));
 
