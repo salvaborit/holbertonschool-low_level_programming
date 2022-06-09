31526fbb1421f0de5fdd0222a7fc5272a1157b74
Salvador Borit <5084@holbertonschool.com>
2022-06-08 19:56:48 -0700

    4.6

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 /**
 * _isalpha - checks for alphabetic character
 *
-* Return: 0 if c is a letter, 1 if otherwise
+* Return: 1 if c is a letter, 0 if otherwise
 */
 int _isalpha(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (0);
-	else if (c >= 'a' && c <= 'z')
-		return (0);
-	else
 		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
