b03dcf88e66ef559077cb32d5059d1e94041b7e4
Salvador Borit <5084@holbertonschool.com>
2022-06-09 06:14:32 -0700

    3.2

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _islower - checks for lowercase letter
-* c is a variable that stores value to check
+* @c: a variable that stores value to check
 * Return: 1 if lowercase, 0 otherwise
 */
 int _islower(int c)
