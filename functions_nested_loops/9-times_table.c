b0e98e67fcec8052e01efc0555a4b128bc72d5bc
Salvador Borit <5084@holbertonschool.com>
2022-06-09 13:51:14 -0700

    9.2

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,8 +12,10 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
+		{
 		_putchar((num * mult) + '0');
 		_putchar(44);
 		_putchar(32);
+		}
 	}
 }
