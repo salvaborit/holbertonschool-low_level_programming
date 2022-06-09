2c9cecd4114c6cf35a83238675e1f648bbb20379
Salvador Borit <5084@holbertonschool.com>
2022-06-09 15:07:42 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result
+* @a: var that stores first int to be added
+* @b: var that stores second int to be added
+* Return: result of addition
+*/
+int add(int a, int b);
+{
+	int a, b, res;
+
+	res = a + b;
+	return(res);
+}
