09e8f6f2f6dc345b201356bef7275221d0d278e2
Salvador Borit <5084@holbertonschool.com>
2022-06-08 19:46:53 -0700

    4

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
 * _islower - checks for lowercase letter
-* c variable stores value to check
+* c is a variable that stores value to check
 * Return: 1 if lowercase, 0 otherwise
 */
 int _islower(int c)
 {
+	int c;
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+*
+* Return: 0 if c is a letter, 1 if otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' && c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (0);
+}
