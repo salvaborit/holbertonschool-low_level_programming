c8d45765d884087304a4f7f6b7aa9a3c2af2d03f
Salvador Borit <5084@holbertonschool.com>
2022-06-09 14:13:07 -0700

    9.7

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,13 +9,27 @@ void times_table(void)
 	int num;
 	int mult;
 	int res;
+	int dig1;
+	int dig2;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
 		res = num * mult;
-		_putchar(res + '0');
+		dig1 = res / 10;
+		dig2 = res % 10;	
+		if (dig1 == 0)
+		{
+		_putchar(32);
+		_putchar(dig2 + '0');
+		_putchar(44);
+		_putchar(32);
+		}
+		else
+		{
+		_putchar(dig1 + '0');
+		_putchar(dig2 + '0');
 		_putchar(44);
 		_putchar(32);
 		}
