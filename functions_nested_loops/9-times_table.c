bb6965ee532ad6e547627380066d12d97bd8f26a
Salvador Borit <5084@holbertonschool.com>
2022-06-09 14:45:48 -0700

    9.15

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,46 +9,42 @@ void times_table(void)
 	int num;
 	int mult;
 	int res;
-	int dig1;
-	int dig2;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
 			res = num * mult;
-			dig1 = res / 10;
-			dig2 = res % 10;	
-			if (dig1 == 0)
+			if (res / 10 == 0)
 			{
 				if (mult == 0)
 				{
-					_putchar(dig2 + '0');
-						if (mult < 9)
-							_putchar(44);
-						else
-							_putchar('\n');
+					_putchar((res % 10) + '0');
+					if (mult < 9)
+						_putchar(44);
+					else
+						_putchar('\n');
 				}
 				else
 				{
 					_putchar(32);
 					_putchar(32);
-					_putchar(dig2 + '0');
-						if (mult < 9)
-							_putchar(44);
-						else
-							_putchar('\n');
+					_putchar((res % 10) + '0');
+					if (mult < 9)
+						_putchar(44);
+					else
+						_putchar('\n');
 				}
 			}
 			else
 			{
 			_putchar(32);
-			_putchar(dig1 + '0');
-			_putchar(dig2 + '0');
-				if (mult < 9)
-					_putchar(44);
-				else
-					_putchar('\n');
+			_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
+			if (mult < 9)
+				_putchar(44);
+			else
+				_putchar('\n');
 			}
 		}
 	}
