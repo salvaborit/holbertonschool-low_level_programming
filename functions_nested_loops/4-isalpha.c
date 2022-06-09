34ae6f14c636e46d481110db1f2e96c5459c06aa
Salvador Borit <5084@holbertonschool.com>
2022-06-08 19:55:26 -0700

    4.5

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,7 +6,9 @@
 */
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+	else if (c >= 'a' && c <= 'z')
 		return (0);
 	else
 		return (1);
