6ad11bbcf026ff1ebb59459963b119289abe5b36
Salvador Borit <5084@holbertonschool.com>
2022-06-09 06:33:36 -0700

    5.1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,22 +9,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(44);
-		_putchar(32);
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		_putchar(44);
-		_putchar(32);
 		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		_putchar(44);
-		_putchar(32);
 		return (0);
 	}
 }
