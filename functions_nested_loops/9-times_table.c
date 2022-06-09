aec8fd0ec3426188b834b8cc5d76ddef1305282a
Salvador Borit <5084@holbertonschool.com>
2022-06-09 13:45:20 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* times_table - prints the 9 times table
+*
+* Return: void
+*/
+void times_table(void)
+{
+	int num;
+	int mult;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (mult = 0; mult <= 9; mult++)
+		_putchar((num * mult) + '0');
+		_putchar(44);
+		_putchar(32);
+	}
+}
