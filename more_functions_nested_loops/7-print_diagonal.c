016134ff1928510291a559f927da95916c8843ff
Salvador Borit <5084@holbertonschool.com>
2022-06-10 13:48:00 -0700

    7.2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,18 @@ void print_diagonal(int n)
 	int s, t;
 
 	t = 0;
-	for (; n > 0; n--)
+	if (n > 0)
 	{
-		for (s = 0; s <= t ; s++)
-			if (s != 0)
-				_putchar(32);
-		_putchar(92);
-		_putchar('\n');
-		t++;
+		for (; n > 0; n--)
+		{
+			for (s = 0; s <= t ; s++)
+				if (s != 0)
+					_putchar(32);
+			_putchar(92);
+			_putchar('\n');
+			t++;
+		}
 	}
+	else
+		_putchar('\n');
 }
