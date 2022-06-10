c0b6d90e69615772a4e3714805a0edeebf7d11cd
Salvador Borit <5084@holbertonschool.com>
2022-06-10 09:33:31 -0700

    5.3

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,15 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (num = 0; num <= (14 + '0'); num++)
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
 			_putchar(num + '0');
+		}
 		_putchar('\n');
 	}
 }
