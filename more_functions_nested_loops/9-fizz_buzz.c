e19911f0e6c66aacc16d16cc01c33f827500b475
Salvador Borit <5084@holbertonschool.com>
2022-06-10 15:52:01 -0700

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* fizzbuzz - fizzbuzz
+*/
+void fizzbuzz(void)
+{
+	int i;
+
+	for (i = 0; i <=100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz", i);
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+	}
+}
