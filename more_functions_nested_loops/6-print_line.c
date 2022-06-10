141df8b02ecabf5a6b7c34559da616a003064a17
Salvador Borit <5084@holbertonschool.com>
2022-06-10 12:36:58 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - draws a straight line in the terminal for n spaces
+* @n: var that stores number os spaces line will measure
+*/
+void print_line(int n);
+{
+	if (n <= 0)
+		_putchar('\n');
+	for (; n > 0; n--)
+	{
+		_putchar(95):
+		_putchar('\n');
+	}
+}
