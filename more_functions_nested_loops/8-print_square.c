d325bec9c7f5c6aa9d492dc7bc9e05c3bb28eb94
Salvador Borit <5084@holbertonschool.com>
2022-06-10 15:26:50 -0700

    8.1

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
 * print_square - prints a square of # (hash) on the terminal
-*
-* Return: Always 0 (Success)
+* @size: var that stores size of square
 */
 void print_square(int size)
 {
@@ -11,7 +10,7 @@ void print_square(int size)
 	if (size <= 0)
 		_putchar('\n');
 	else
-		for (i = 0; i <= size; i++)
-			for (j = 0; j <= size; j++)
-				_putchar(35);
+		for (i = 0; i < size; i++)
+			_putchar(35);
+	_putchar('\n');
 }
