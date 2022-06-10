e002f07d32dc55700af7d4c8fb6c5920c94cb3e2
Salvador Borit <5084@holbertonschool.com>
2022-06-10 16:37:11 -0700

    9.16

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,21 +6,22 @@ int main(void)
 {
 	int i, fizz, buzz, fizzbuzz;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		fizz = i % 3;
 		buzz = i % 5;
 		fizzbuzz = i % 15;
-		if (i == 0)
+		if (i == 1)
 			printf("%d", i);
-		else if (fizzbuzz == 0 && i != 0)
+		else if (fizzbuzz == 0)
 			printf(" FizzBuzz");
-		else if (buzz == 0 && i != 0)
+		else if (buzz == 0)
 			printf(" Buzz");
-		else if (fizz == 0 && i != 0)
+		else if (fizz == 0)
 			printf(" Fizz");
 		else
 			printf(" %d", i);
 	}
+	printf("\n");
 	return (0);
 }
