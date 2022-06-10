0e92986a2591712cfe65e921fd6c092b21df62f2
Salvador Borit <5084@holbertonschool.com>
2022-06-10 07:17:42 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers 0-14 10 times
+*/
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar(num + '0');
+		_putchar('\n');
+	}
+}
