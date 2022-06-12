86d87b34ddfd0337a11716c9c729952004a327b4
Salvador Borit <5084@holbertonschool.com>
2022-06-11 21:48:23 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_triangle - prints a triangle
+* @size: var that stores value of triangle size
+*/
+void print_triangle(int size);
+{
+	int spaces, hash;
+
+	spaces = size - 1;
+	hash = 1;	
+	for (; size >= 0; size--)
+	{
+		for (i = 0; i <= spaces; i++)
+		{
+			_putchar(32);
+		}
+		spaces--;
+		for (j = 0; j <= hash; j++)
+		{
+			_putchar(35);
+		}
+		hash++;
+		_putchar('\n');
+	}
+}
