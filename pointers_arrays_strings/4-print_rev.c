1258964d4d2487e38aad866811a58d312a858c35
Salvador Borit <5084@holbertonschool.com>
2022-06-14 18:07:32 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - prints string in reverse
+* @s: string to be printed in reverse
+*/
+void print_rev(char *s)
+{
+	int len, i;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (i = len - 1; s[i] >= s[0]; i--)
+		printf("%c", s[i]);
+	printf("\n");
+}
