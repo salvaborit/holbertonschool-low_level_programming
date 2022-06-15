949ef2491f8c7dfd672f76eaa7d2127dfb8efb39
Salvador Borit <5084@holbertonschool.com>
2022-06-15 10:39:56 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rev_string - reverses a string
+* @s: string to be reversed
+*/
+void rev_string(char *s)
+{
+	int length, *firstDig, *lastDig, temp;
+
+	for (length = 0; s[length] != '\0'; length++){}
+	firstDig = s;
+	lastDig = s;
+	for (i = 0; i < length - 1; i++)
+		lastDig++;
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = *lastDig;
+		*lastDig = *firstDig;
+		*firstDig = *temp
+		firstDig++;
+		lastDig--;
+	}
+}
