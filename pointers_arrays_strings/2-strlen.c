2180701c82c71c37f0335d797bb5b6de13ff78ed
Salvador Borit <5084@holbertonschool.com>
2022-06-14 17:16:41 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* strlen - returns length of string
+*
+* Return: length of string
+*/
+int _strlen(char *s);
+{
+	for (i = 0; s[i] != 0; i++)
+		i++
+	return (i);
+}
