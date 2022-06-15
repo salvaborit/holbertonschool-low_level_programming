0c56a48795b2afcb42db21691e596a253f516c85
Salvador Borit <5084@holbertonschool.com>
2022-06-15 10:59:52 -0700

    6.1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,11 @@
 */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; str[i] != '\0'; i = i + 2)
+	for (len = 0; str[len] != '\0'; len++)
+	{}
+	for (i = 0; i < len; i = i + 2)
 	{
 		_putchar(str[i]);
 	}
