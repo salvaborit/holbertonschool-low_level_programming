6b0504b10c43d6db1edac560bf1f976b44325b15
Salvador Borit <5084@holbertonschool.com>
2022-06-16 08:44:33 -0700

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* main - Entry point
+* @src: pointer to string to be appended
+* @dest: pointer to string that will have *src appended
+* Return: pointer to string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int lenDest, lenSrc, i, j;
+
+	for (lenDest = 0; dest[lenDest] != '\0'; lenDest++)
+	{}
+	for (lenSrc = 0; src[lenSrc] != '\0'; lenSrc++)
+	{}
+	j = 0;
+	for (i = lenDest + 1; i < lenSrc; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
