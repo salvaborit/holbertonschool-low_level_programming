3d14a67485c5b767c8fc818b9e0f57a8c30fd2e6
Salvador Borit <5084@holbertonschool.com>
2022-06-16 15:15:31 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* _strncat - concatenates 2 strings using n bytes from src
+* @src: source string
+* @dest: destination string
+* @n: bytes to be copied
+* Return: pointer to string dest
 */
-int main(void)
+char *_strncat(char *dest, char *src, int n)
 {
-	return (0);
+	int i, j;
+
+	for (j = 0; dest[j] != 0; j++)
+	{}
+	for (i = 0; i < n; i++)
+	{
+		dest[j] = src[i];
+	}
+	return (dest);
 }
