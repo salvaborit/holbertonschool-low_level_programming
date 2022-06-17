f29159570e5b1fcf3fdebcea323c8afeaa1c71ef
Salvador Borit <5084@holbertonschool.com>
2022-06-17 11:42:02 -0700

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* reverse_array - reverses the content of an array of integers
+* @a: pointer to array of integers
+* @n: number of elements in array
 */
-int main(void)
+void reverse_array(int *a, int n)
 {
-	return (0);
+	int i, j, temp;
+
+	i = 0;
+	j = n - 1;
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
 }
