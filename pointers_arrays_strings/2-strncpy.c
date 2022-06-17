c853a00be3bb5c5b8dd6b1080927365a3222bd29
Salvador Borit <5084@holbertonschool.com>
2022-06-17 08:31:27 -0700

    2.2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,8 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	/* copies src to dest */
 	for (i = 0; i < n && src[i] != 0; i++)
-		dest[i] = dest[i];
+		dest[i] = src[i];
+	/* if any extra bytes available in dest, fill with '\0' */
 	for (; i < n; i++)
 		dest[i] = 0;
 	return (dest);
