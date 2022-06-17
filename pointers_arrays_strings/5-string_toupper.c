b130d6f2393e300a4e983339effed7ee72a68fbe
Salvador Borit <5084@holbertonschool.com>
2022-06-17 14:56:35 -0700

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
+* string_toupper - changes all lowercase letters of a string to uppercase
+* 
 * Return: Always 0 (Success)
 */
-int main(void)
+char *string_toupper(char *c)
 {
-	return (0);
+	int i;
+
+	i = 0;
+	while (c[i])
+		if (c[i] < 122 && c[i] > 97)
+			c[i] = c[i] - 32;
+	return (c);
+	
 }
