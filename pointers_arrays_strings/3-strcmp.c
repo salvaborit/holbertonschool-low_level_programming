18fd2ad5b4d8d7665dccf389650849e32d446310
Salvador Borit <5084@holbertonschool.com>
2022-06-17 11:13:39 -0700

    3.3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,16 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, ret;
+	int i;
 
 	i = 0;
-	while (s1[i] == s2[i])
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
-	if (s1[i] > s2[i] || s2[i] == '\0')
-		ret = s1[i] - s2[i];
-	else if (s1[i] < s2[i] || s1[i] == '\0')
-		ret = s2[i] - s1[i];
-	else
-		ret = 0;
-	return (ret);
+	}
+	if (s2[i])
+		return (s2[i]);
+	return (0);
 }
