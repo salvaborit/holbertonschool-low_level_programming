90fd0d521d791281e51778ddc2468e6cfd788379
Salvador Borit <5084@holbertonschool.com>
2022-06-18 12:38:35 -0700

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,21 @@
+#include "main.h"
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* _strchr - locates a character in a string
+* @s: string to be checked
+* @c: character to be locaetd
+* Return: pointer to first occurence of c, 0 if character not found
 */
-int main(void)
+char *_strchr(char *s, char c)
 {
+	int i;
+	char *firstOcc;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] == c)
+		{
+			firstOcc = &s[i];
+			return (firstOcc);
+		}
 	return (0);
 }
