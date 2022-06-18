413c942f8ce600798fa7e3509d53ea50b0275722
Salvador Borit <5084@holbertonschool.com>
2022-06-17 18:57:27 -0700

    6.3

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* leet - encodes a string into 1337 (leet slang)
+* @c: string to be encoded
+* Return: encoded string in leet
 */
-int main(void)
+char *leet(char *c)
 {
-	return (0);
+	int i, j;
+	char *l = "AaEeOoTtLl";
+	char *n = "4433007711";
+
+	for (i = 0; c[i]; i++)
+		for (j = 0; l[j]; j++)
+			if (l[j] == c[i])
+				c[i] = n[j];
+	return (c);
 }
