b80daf85d2d4ee7058e43b354acb495c409b7dac
Salvador Borit <5084@holbertonschool.com>
2022-06-17 18:31:55 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* cap_string - capitalizes words of a string
+* @c: string to be capitalized
+* Return: capitalized string
 */
-int main(void)
+char *cap_string(char *c)
 {
-	return (0);
+	int i;
+
+	for (i = 0; c[i]; i++)
+	{
+		if (i == 0 && c[i] >= 97 && c[i] <= 122)
+			c[i] -= 32;
+		if ((c[i - 1] == 44 || c[i - 1] == 59 || c[i - 1] == 46 
+			|| c[i - 1] == 33 || c[i - 1] == 63 
+			|| c[i - 1] == 34 || c[i - 1] == 40 
+			|| c[i - 1] == 41 || c[i - 1] == 123 
+			|| c[i - 1] == 125 || c[i - 1] == 32 
+			|| c[i - 1] == '\t' || c[i - 1] == '\n')
+			&& (c[i] >= 97 && c[i] <= 122))
+		{
+			c[i] -= 32;
+		}
+	}
+	return (c);
 }
