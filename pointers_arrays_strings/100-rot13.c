fbede696cc2dbc39bd7c81a2a0a8953bed975ad6
Salvador Borit <5084@holbertonschool.com>
2022-06-17 21:53:32 -0700

    8

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rot13 - encodes a string using rot13
+* @c - string to be encoded
+* Return: encoded string
+*/
+char *rot13(char *c)
+{
+	int i, j;
+	char *abc = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *rot = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; c[i]; i++)
+		for (j = 0; abc[j]; j++)
+			if (c[i] == abc[j])
+				c[i] = rot[j];
+	return (c);
+}
