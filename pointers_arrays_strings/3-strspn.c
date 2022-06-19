1f0bf857dfc41a7d14a26741b2cc79a48cd4faa8
Salvador Borit <5084@holbertonschool.com>
2022-06-18 19:46:55 -0700

    3.5

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,11 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, ret;
 
 	for (i = 0; s[i]; i++)
-	{
 		for (j = 0; accept[j]; j++)
-		{
 			if (s[i] == accept[j])
-				continue;
-			break;
-		}
-		break;
-	}
-	return (i);
+				ret++;
+	return (ret);
 }
