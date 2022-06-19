2b4191d377247a1ac0c2ce19a196660ced636aac
Salvador Borit <5084@holbertonschool.com>
2022-06-18 19:19:18 -0700

    4.1

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -13,12 +13,8 @@ char *_strpbrk(char *s, char *accept)
 	int i, j;
 
 	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; accept[j]; i++)
+		for (j = 0; accept[j]; j++)
 			if (s[i] == accept[j])
-			{
 				return (s + i);
-			}
-	}
 	return (0);
 }
