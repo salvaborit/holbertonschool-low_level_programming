266c13ddb3d0d21e90ee1520c184c49550ff33cf
Salvador Borit <5084@holbertonschool.com>
2022-06-18 19:16:30 -0700

    5

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,24 @@
+#include "main.h"
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* _strpbrk - locates first occurrence in the string s of any of the
+* bytes in the string accept
+* @s: string to be checked
+* @accept: string to be compared to
+* Return: pointer to the byte in s that matches byte in accept, or 
+* NULL if no such byte is found
 */
-int main(void)
+char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; i++)
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+	}
 	return (0);
 }
