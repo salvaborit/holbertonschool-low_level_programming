670243aaa2cb4b2c6c7e5131305072f7216fafdb
Salvador Borit <5084@holbertonschool.com>
2022-06-20 16:47:19 -0700

    6.1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -18,11 +18,11 @@ int prm(int n, int i)
 {
 	if (i == n)
 		return (1);
+	if (n == 1)
+		return (1);
 	if (n <= 0)
 		return (0);
-	if (n <= 2)
-		return (1);
 	if (n % i == 0)
 		return (0);
-	return(prm(n, i + 1));
+	return (prm(n, i + 1));
 }
