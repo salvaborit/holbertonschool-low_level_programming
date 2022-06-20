f5319113fb6494028e6783f77723c2e6a05082a7
Salvador Borit <5084@holbertonschool.com>
2022-06-19 18:59:38 -0700

    7.5

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,24 +7,22 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int i, j, frontSum, backSum;
+	int i, frontSum, backSum;
 
 	/* front diag */
 	i = 0;
-	j = 0;
-	while (i < size)
+	frontSum = 0;
+	while (i < size * size)
 	{
-		frontSum = frontSum + a[j];
-		i++;
-		j = j + (size + 1);
+		frontSum = frontSum + a[i];
+		i = i + (size + 1);
 	}
-	i = 0;
-	j = size - 1;
-	while (i < size - 1)
+	i = size - 1;
+	backSum = 0;
+	while (i < size * size)
 	{
-		backSum = backSum + a[j];
-		i++;
-		j = j + (size - 1);
+		backSum = backSum + a[i];
+		i = i + (size - 1);
 	}
 	printf("%d, %d", frontSum, backSum);
 }
