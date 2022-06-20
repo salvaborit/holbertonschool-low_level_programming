ca9f41e51f8d8ae0a6c1f87474a3e839c6232567
Salvador Borit <5084@holbertonschool.com>
2022-06-20 16:42:36 -0700

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* is_prime_number - checks if a number is prime
+* @n: number to check if prime
+* Return: 1 if prime, 0 if otherwise
 */
-int main(void)
+int is_prime_number(int n)
 {
-	return (0);
+	return (prm(n, 2));
+}
+/**
+* prm - checks if a number is prime
+* @n: number to check if prime
+* @i: numbers that n will be divided by to check if prime
+* Return: 1 if prime, 0 if otherwise
+*/
+int prm(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if (n <= 0)
+		return (0);
+	if (n <= 2)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return(prm(n, i + 1));
 }
