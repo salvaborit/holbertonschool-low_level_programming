a7be0dc37a69a0e8c82ace975f99cd7de43c1e1e
Salvador Borit <5084@holbertonschool.com>
2022-06-21 12:18:28 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints arguments passed to CL
+* @argc - number of arguments passed to CL
+* @argv - unused
+* Return: Always 0 (Success)
+*/
+int main(int argc, char  __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
