57a61cfd9496d8c594fa59819a0211e3814c31b7
Salvador Borit <5084@holbertonschool.com>
2022-06-21 09:25:03 -0700

    1.2

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # creates a static library called liball. from all the .c files in the current dir
 
-gcc -c -Wall -Werror -Wextra *.c | ar -rc liball.a *.o
+gcc -c -Wall -Werror -Wextra *.c
+ar -rc liball.a *.o
