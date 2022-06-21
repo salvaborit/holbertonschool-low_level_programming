504baf1f85b802e5a77952189a8fa588015e650f
Salvador Borit <5084@holbertonschool.com>
2022-06-21 12:03:58 -0700

    0.2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* whatsmyname - prints its name
+* main - prints its name
 * @argv: array that stores arguments passed to command line
 */
-void whatsmyname(char* argv[])
+int main(int  __attribute__ ((unused)) argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
 }
