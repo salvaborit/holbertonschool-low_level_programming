60a12c7add32d2324beeab4c1b52bcf104f3ff6e
Salvador Borit <5084@holbertonschool.com>
2022-06-21 12:09:29 -0700

    0.5

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
 * @argc: unused
 * Return: always 0, success
 */
-int main(int  __attribute__ ((unused)) argc, char* argv[])
+int main(int  __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
