70a8407f556cfcdff242cc60fb6f9e67a4058b26
Salvador Borit <5084@holbertonschool.com>
2022-06-21 12:08:48 -0700

    0.4

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,9 +2,11 @@
 /**
 * main - prints its name
 * @argv: array that stores arguments passed to command line
+* @argc: unused
+* Return: always 0, success
 */
 int main(int  __attribute__ ((unused)) argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
