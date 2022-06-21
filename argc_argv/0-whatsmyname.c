82f2fb649eb45e74465eeec9904902bac1c59f92
Salvador Borit <5084@holbertonschool.com>
2022-06-21 11:49:58 -0700

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void main(int charc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains the C - argc/argv project files for Holberton.
