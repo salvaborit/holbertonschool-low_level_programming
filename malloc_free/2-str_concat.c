bbd99b5fa73c0e4320efb6a9f9df7ce9630d5846
Salvador Borit <5084@holbertonschool.com>
2022-06-23 13:37:26 -0700

    2.1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,23 +13,31 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	int lenStr, i, j;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	lenStr = strlen(s1);
-	lenStr += strlen(s2);
+	if (s1 != NULL && s2 == NULL)
+		lenStr = strlen(s1);
+	if (s1 == NULL && s2 != NULL)
+		lenStr = strlen(s2);
+	if (s1 != NULL && s2 != NULL)
+	{
+		lenStr = strlen(s1);
+		lenStr += strlen(s2);
+	}
 	ar = malloc(lenStr);
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
-	{
-		ar[j] = s1[i];
-		j++;
-	}
-	for (i = 0; s2[i]; i++)
-	{
-		ar[j] = s2[i];
-		j++;
-	}
+	j = 0;
+	if (s1 != NULL)
+		for (i = 0; s1[i]; i++)
+		{
+			ar[j] = s1[i];
+			j++;
+		}
+	if (s2 != NULL)
+		for (i = 0; s2[i]; i++)
+		{
+			ar[j] = s2[i];
+			j++;
+		}
 	ar[j] = '\0';
 	return (ar);
 }
