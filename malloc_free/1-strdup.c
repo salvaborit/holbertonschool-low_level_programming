0fc41ca3c35d4f60f8906567f23fea0a3656554d
Salvador Borit <5084@holbertonschool.com>
2022-06-23 12:49:11 -0700

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 /**
 * _strdup - fills newly allocated space in memory with a string
 * @str: string given to fill newly allocated space in memory
@@ -8,5 +9,14 @@
 */
 char *_strdup(char *str)
 {
+	char *ar = malloc(strlen(str));
+	int i, len;
 
+	len = strlen(str);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ar[i] = str[i];
+	ar[i] = '\0';
+	return (ar);
 }
