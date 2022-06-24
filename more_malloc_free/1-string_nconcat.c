faea0bdeebf2cc0813a7c1000406291d34f691be
Salvador Borit <5084@holbertonschool.com>
2022-06-24 15:52:37 -0700

    1.5

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,12 +30,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	i = 0;
 	j = 0;
-	if (strlen(s1) > 0 && s1 != NULL)
+	if (strlen(s1) > 0 || s1 != NULL)
 		for (; s1[i]; i++)
 			ar[i] = s1[i];
-	if (strlen(s2) > 0 && s2 != NULL)
-		for (; j < n; i++, j++)
-			ar[i] = s2[j];
+	if (strlen(s2) > 0 || s2 != NULL)
+	{
+		if (n > strlen(s2))
+			for (; s2[j]; i++, j++)
+				ar[i] = s2[j];
+		else
+			for (; j < n; i++, j++)
+				ar[i] = s2[j];
+	}
 	ar[i] = '\0';
 	return (ar);
 }
