9e6025fbe12cb4517556da105603259c5ed11140
Salvador Borit <5084@holbertonschool.com>
2022-06-24 17:02:02 -0700

    1.6

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lenStr, i, j;
 
 	lenStr = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (strlen(s1) > 0)
 		lenStr += strlen(s1);
 	if (strlen(s2) > 0)
