7c8ba55f71c1e86c7c643e7c5c85210443867ab5
Salvador Borit <5084@holbertonschool.com>
2022-06-28 10:48:42 -0700

    0

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - Entry point
+* 
+* Return: 
+*/
+int main(void)
+{
+	
+}
---
+++ preprocessor/README.md
@@ -0,0 +1,1 @@
+This directory contains the C - preprocessor project files for Holberton.
