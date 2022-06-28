ed012769481216109b03bce97112d71af8b42cea
Salvador Borit <5084@holbertonschool.com>
2022-06-28 15:01:33 -0700

    4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,14 +4,25 @@
 #include "dog.h"
 /**
 * new_dog - creates a new dog_t
-* 
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
 * Return: pointer to new dog_t
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t dogg, *p;
 	unsigned int i;
+	int nameLen, ownLen;
 
+	nameLen = strlen(name);
+	ownLen = strlen(owner);
+	dogg.name = malloc(nameLen);
+	if (!dogg.name)
+		return (NULL);
+	dogg.owner = malloc(ownLen);
+	if (!dogg.owner)
+		return (NULL);
 	for (i = 0; i < strlen(name); i++)
 		dogg.name[i] = name[i];
 	dogg.age = age;
