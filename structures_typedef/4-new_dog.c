4c8d1da2f6b9143fce884dd142afc62c3c7a21a7
Salvador Borit <5084@holbertonschool.com>
2022-06-28 14:08:30 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
+#include "dog.h"
 /**
-* main - Entry point
+* new_dog - creates a new dog_t
 * 
-* Return: 
+* Return: pointer to new dog_t
 */
-int main(void)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	dog_t dogg, *p;
+	unsigned int i;
+
+	for (i = 0; i < strlen(name); i++)
+		dogg.name[i] = name[i];
+	dogg.age = age;
+	for (i = 0; i < strlen(owner); i++)
+		dogg.owner[i] = owner[i];
+	p = &dogg;
+	return (p);
 }
