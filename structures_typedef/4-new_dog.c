f9a83bdeb96f96336bdc8b97d74e5a057e37fb75
Salvador Borit <5084@holbertonschool.com>
2022-06-28 15:02:53 -0700

    4.2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,7 +22,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dogg.owner = malloc(ownLen);
 	if (!dogg.owner)
+	{
+		free(dogg.name);
 		return (NULL);
+	}
 	for (i = 0; i < strlen(name); i++)
 		dogg.name[i] = name[i];
 	dogg.age = age;
