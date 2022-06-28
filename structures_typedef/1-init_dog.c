d8462f8abb754b84e38559351eb51b71d6811696
Salvador Borit <5084@holbertonschool.com>
2022-06-28 12:57:36 -0700

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
 /**
-* main - Entry point
-* 
-* Return: 
+* init_dog - initializes dog struct
+* @d: pointer to new dog struct created
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
 */
-int main(void)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog - structure of type dog
 * @name: name of dog
 * @age: age of dog
 * @owner: owner of dog
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
