b3e1c5ab60ef08e14b17cf5b961a50d856cd4f92
Salvador Borit <5084@holbertonschool.com>
2022-06-29 14:08:34 -0700

    2.6

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL)
-		return (3);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
 	for (i = 0; array[i]; i++)
 	{
 		if (cmp(array[i]))
