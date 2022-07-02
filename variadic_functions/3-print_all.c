a66a09410ddf2399e3f4d94fd3ae9bd49015c318
Salvador Borit <5084@holbertonschool.com>
2022-07-02 12:17:48 -0700

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,12 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#include "variadic_functions.h"
 /**
-* main - Entry point
-* 
-* Return: 
+* print_all - prints anything
+* @format - string (constant)
 */
-int main(void)
+void print_all(const char * const format, ...)
 {
-	
+	va_list ap;
+	int i, n, cond, formatLen;
+	char c;
+
+	i = 0;
+	n = 0;
+	while (format[i])
+	{
+		c = format[i];
+		if (c == 'c' || c == 'i' || c == 'f' || c == 's')
+			n++;
+		i++;
+	}
+
+	va_start(ap, format);
+	i = 0;
+	formatLen = strlen(format);
+	while (format[i])
+	{
+		cond = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				cond = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				cond = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				cond = 1;
+				break;
+			case 's':
+				printf("%s", va_arg(ap, char *));
+				cond = 1;
+				break;
+		}
+		if (cond == 1 && i < formatLen - 1)
+			printf(", ");
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
