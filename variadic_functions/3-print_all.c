4682e87fc72550c56e7b8bb3c2fdc4136e840704
Salvador Borit <5084@holbertonschool.com>
2022-07-03 15:10:26 -0700

    3.10

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,39 +9,36 @@ void print_all(const char * const format, ...)
 	int i, cond, formatLen;
 	char *str;
 
-	va_start(ap, format);
 	i = 0;
-	formatLen = strlen(format);
-	if (format != 0)
+	formatLien = strlen(format);
+	va_start(ap, format);
+	while (format && format[i])
 	{
-		while (format[i])
+		cond = 0;
+		switch (format[i])
 		{
-			cond = 0;
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(ap, int));
-					cond = 1;
-					break;
-				case 'i':
-					printf("%d", va_arg(ap, int));
-					cond = 1;
-					break;
-				case 'f':
-					printf("%f", va_arg(ap, double));
-					cond = 1;
-					break;
-				case 's':
-					str = va_arg(ap, char *);
-					!str ? printf("(nil)") : printf("%s", str);
-					cond = 1;
-					break;
-			}
-			if (cond == 1 && i < formatLen - 1)
-				printf(", ");
-			i++;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				cond = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				cond = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				cond = 1;
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				!str ? printf("(nil)") : printf("%s", str);
+				cond = 1;
+				break;
 		}
+		if (cond == 1 && i < formatLen - 1)
+			printf(", ");
+		i++;
 	}
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
