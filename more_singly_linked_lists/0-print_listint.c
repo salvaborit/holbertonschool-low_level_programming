4f5e6b16a5698b2094943d16e78d48270dca7636
Salvador Borit <5084@holbertonschool.com>
2022-07-14 19:26:00 -0300

    0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* print_listint - prints all the elements of a listint_t linked list
+* @h: list
+* Return: number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains the C - more singly linked lists project files for Holberton.
