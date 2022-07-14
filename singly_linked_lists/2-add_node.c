8caa053a07a3a073dc53d8b82b3f5edfb9a395c9
Salvador Borit <5084@holbertonschool.com>
2022-07-13 23:30:22 -0300

    mig

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,8 +1,20 @@
 #include "lists.h"
 /**
-* 
+* add_node - adds a new node at the beginning of a linked list
+* @head: points to first element
+* @str: string to add to list
+* Return: address of new element
 */
-
+list_t *add_node(list_t **head, const char *str)
 {
-	
+	unsigned int i;
+	list_t *newHead;
+	list_t newNode;
+		
+	newNode = malloc(sizeof(list_t));
+	if (newNode != NULL)
+		return (NULL);
+	newNode = {str, strlen(str), **head};
+	newHead = newNode->str;
+	return (newHead);
 }
