c45288577f0466f82e127beb892ad8183c5b06a7
Salvador Borit <5084@holbertonschool.com>
2022-07-14 19:39:55 -0300

    1

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,14 @@
-#include ".h"
+#include "lists.h"
 /**
-* 
+* listint_len - returns the number of elements in a linked listint_t list
+* @h: ll
+* Return: number of elements in ll
 */
-
+size_t listint_len(const listint_t *h)
 {
-	
+	size_t i = 0;
+
+	while (h)
+		i++;
+	return (i);
 }
