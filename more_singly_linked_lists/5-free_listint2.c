c039aabf91e132348f5631bbe5482a3bcd9ba0a9
Salvador Borit <5084@holbertonschool.com>
2022-07-15 17:28:52 -0300

    5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,19 @@
-#include ".h"
+#include "lists.h"
 /**
-* 
+* free_listint2 - frees a listint_t list
+* @head: pointer to first node
 */
-
+void free_listint2(listint_t **head)
 {
+	listint_t *temp;
 	
+	if (!head)
+		return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(head);
+		*head = temp->next;
+	}
+	head = NULL;
 }
