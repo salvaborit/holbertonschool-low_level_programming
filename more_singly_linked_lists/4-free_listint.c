e9e23a95e88c3d95d3b13d092cf9aae631765399
Salvador Borit <5084@holbertonschool.com>
2022-07-15 16:21:36 -0300

    4

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,16 @@
-#include ".h"
+#include "lists.h"
 /**
-* 
+* free_listint - frees a listint_t list
+* @head: pointer to first node
 */
-
+void free_listint(listint_t *head)
 {
-	
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
