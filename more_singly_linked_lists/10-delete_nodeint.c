cd081c0003f70e018c695296208f4dfc38e4d111
Salvador Borit <5084@holbertonschool.com>
2022-07-15 22:13:26 -0300

    10.2

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < index - 1 && temp; i++)
 	{
 		if (temp->next == NULL)
 			return (-1);
