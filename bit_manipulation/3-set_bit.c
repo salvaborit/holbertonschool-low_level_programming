8db1e6a694d1ad6ffc526278adbc46179a569ba5
Salvador Borit <5084@holbertonschool.com>
2022-07-19 19:39:40 -0300

    3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,8 +1,17 @@
-#include ".h"
+#include "main.h"
 /**
-* 
+* set_bit - sets value of a bit to 1 at a given index
+* @n: pointer to num
+* @index: position of bit to set
+* Return: 1 if success, -1 if error
 */
-
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int pn = *n, mask = 1;
+
+	mask <<= index;
+	if (index < 64 || n)
+		*n = pn | mask;
+	return (-1);
+
 }
