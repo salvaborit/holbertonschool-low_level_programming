2431009956819950b3324274c82e81fd3da00bc5
Salvador Borit <5084@holbertonschool.com>
2022-07-19 18:33:40 -0300

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,8 +1,16 @@
-#include ".h"
+#include "main.h"
 /**
-* 
+* print_binary - prints the binary representation of a number
+* @n: integer to convert to binary
 */
-
+void print_binary(unsigned long int n)
 {
-	
+	if (n == 1 || n == 0)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+	return;
 }
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -10,6 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
+int _putchar(char c);
 unsigned int _pow(unsigned int num, int exp);
 
 #endif /* MAIN_H */
