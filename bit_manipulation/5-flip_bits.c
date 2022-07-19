addf830b174d7ffbc1615b4f02ec4bda1f8c006d
Salvador Borit <5084@holbertonschool.com>
2022-07-19 20:53:23 -0300

    5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,8 +1,21 @@
-#include ".h"
+#include "main.h"
 /**
-* 
+* flip_bits - returns number of bits to flip to get from one num to another
+* @n: src number
+* @m: dest num
+* Return: number of bits to flip
 */
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	int i;
+	unsigned long int c;
+
+	for (i = 0, c = 0; i < 64; i++)
+	{
+		if ((n & 1) != (m & 1))
+			c++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return(c);
 }
