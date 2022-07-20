c0eff2921bb3a922d8dbbf594d544910c08b5e02
Salvador Borit <5084@holbertonschool.com>
2022-07-20 16:52:30 -0300

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,5 +7,13 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	
+	int fd; 
+
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+	if (!filename || fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+	return (1);
 }
