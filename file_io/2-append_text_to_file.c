16ec2be075a64c2e7aa25f71cebbf27b6720a5b4
Salvador Borit <5084@holbertonschool.com>
2022-07-20 17:34:00 -0300

    2.5

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,8 +9,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int w, fd;
 
+	if (!filename)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (!filename || fd == -1)
+	if (fd == -1)
 		return (-1);
 	if (text_content)
 		w = write(fd, text_content, strlen(text_content));
