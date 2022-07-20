1af82f6ede76a9a907bb3a1f894ad127ba7529d4
Salvador Borit <5084@holbertonschool.com>
2022-07-20 17:20:56 -0300

    2.1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,6 +12,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename || fd == -1)
 		return (-1);
 	if (text_content)
-		write(fd, text_content, strlen(text_content));
+		w = write(fd, text_content, strlen(text_content));
+	if (w < 0)
+		return (-1);
+	close(fd);
 	return (1);
 }
