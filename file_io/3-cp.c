7f8ce2dcffa8e1f5da90ed7c447861a6aeab1092
Salvador Borit <5084@holbertonschool.com>
2022-07-20 21:55:56 -0300

    3.1

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fdFrom = open(av[1], O_RDONLY);
@@ -21,7 +21,7 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdTo = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fdTo = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fdTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
