229d95d2e8db9800d38ec298d17ecb9adae7f891
Salvador Borit <5084@holbertonschool.com>
2022-07-21 17:50:15 -0300

    1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,14 @@
-#include ".h"
+#include "lists.h"
 /**
-* 
+* dlistint_len - returns number of elements in a dlistint_t list
+* @h: pointer to head node
+* Return: number of elements in linked list
 */
-
+size_t dlistint_len(const dlistint_t *h)
 {
-	
+	int i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
 }
