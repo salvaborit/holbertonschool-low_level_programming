f15c5571bbf22dab1999153c4dfccf0db6c578db
Salvador Borit <5084@holbertonschool.com>
2022-07-22 19:10:52 -0300

    4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,16 @@
-#include ".h"
+#include "lists.h"
 /**
-* 
+* free_dlistint - frees a dlistint_t list
+* @head: pointer to head node
 */
-
+void free_dlistint(dlistint_t *head)
 {
-	
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;	
+	}
 }
