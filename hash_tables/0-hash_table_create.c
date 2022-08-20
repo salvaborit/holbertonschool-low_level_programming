9c8fb015d4b63d299d9533c309ca86f021b20386
Salvador Borit <5084@holbertonschool.com>
2022-08-20 19:32:20 -0300

    creates table corectly

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newTable;
+
+	newTable = malloc(sizeof(hash_table_t));
+	if (!newTable)
+		return (NULL);
+
+	newTable->size = size;
+	newTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (newTable->array == NULL)
+	{
+		free(newTable);
+		return (NULL);
+	}
+	return (newTable);
+}
