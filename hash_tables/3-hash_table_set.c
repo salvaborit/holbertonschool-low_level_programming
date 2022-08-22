5a8d56b323ef8ac83376157144262a411e2b96d4
Salvador Borit <5084@holbertonschool.com>
2022-08-21 21:14:49 -0300

    betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!key || !ht)
 		return (0);
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	/* create new node */
 	newNode = malloc(sizeof(hash_node_t));
 	if (!newNode)
