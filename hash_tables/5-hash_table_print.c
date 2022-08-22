439462a835fc8a9cd725fe63e777d15394b84ace
Salvador Borit <5084@holbertonschool.com>
2022-08-21 21:48:39 -0300

    betty

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,25 +7,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index = 0, printCount = 0;
-    hash_node_t *slot = NULL;
+	unsigned long int index = 0, printCount = 0;
+	hash_node_t *slot = NULL;
 
-    if (!ht)
-        return;
+	if (!ht)
+		return;
     
-    putchar('{');
-    while (index < ht->size)
-    {
-        slot = ht->array[index];
-        while (slot)
-        {
-        	if (printCount != 0)
-                printf(", ");
-            printf("'%s': '%s'", slot->key, slot->value);
-            slot = slot->next;
-            printCount++;
-        }
-        index++;
-    }
-    printf("}\n");
+	putchar('{');
+	while (index < ht->size)
+	{
+		slot = ht->array[index];
+		while (slot)
+		{
+			if (printCount != 0)
+				printf(", ");
+			printf("'%s': '%s'", slot->key, slot->value);
+			slot = slot->next;
+			printCount++;
+		}
+		index++;
+	}
+	printf("}\n");
 }
